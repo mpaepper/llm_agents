247515104b4846097333ba12240564a3026540a8
Zetaphor <zetaphor@hey.com>
2023-04-10 07:18:03 -0500

    Add Google search to tools init

--- llm_agents/__init__.py
+++ llm_agents/__init__.py
@@ -3,5 +3,7 @@ from llm_agents.llm import ChatLLM
 from llm_agents.tools.python_repl import PythonREPLTool
 from llm_agents.tools.hackernews import HackerNewsSearchTool
 from llm_agents.tools.search import SerpAPITool
+from llm_agents.tools.google_search import GoogleSearchTool
 
-__all__ = ['Agent', 'ChatLLM', 'PythonREPLTool', 'HackerNewsSearchTool', 'SerpAPITool']
+__all__ = ['Agent', 'ChatLLM', 'PythonREPLTool',
+           'HackerNewsSearchTool', 'SerpAPITool', 'GoogleSearchTool']
